b9aca31e80c94e7d74f1c8e0253a9fe721393a65
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-09-01 22:21:43 +0800

    setup ui

---
+++ app/src/main/java/com/app/expertsubmission/ui/home/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.app.expertsubmission.ui.home
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.app.expertsubmission.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/app/expertsubmission/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.app.expertsubmission.ui.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
