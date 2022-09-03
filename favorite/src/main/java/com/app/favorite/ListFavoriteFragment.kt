7d998959691c510247e9577b30ffb9961438d457
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-09-03 17:47:45 +0800

    show favorite articles

--- favorite/src/main/java/com/app/favorite/ListFavoriteFragment.kt
+++ favorite/src/main/java/com/app/favorite/ListFavoriteFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.core.data.Resource
+import com.app.core.ui.ArticleAdapter
 import com.app.favorite.databinding.FragmentListFavoriteBinding
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.context.loadKoinModules
@@ -14,6 +17,7 @@ class ListFavoriteFragment : Fragment() {
     private var _binding: FragmentListFavoriteBinding? = null
     private val binding get() = _binding!!
     private val viewModel: ListFavoriteViewModel by viewModel()
+    private lateinit var articleAdapter: ArticleAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         loadKoinModules(favoriteModule)
@@ -23,7 +27,17 @@ class ListFavoriteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        articleAdapter = ArticleAdapter {
 
+        }
+        viewModel.getFavoriteArticles().observe(viewLifecycleOwner) {
+            articleAdapter.setData(it)
+        }
+        with(binding) {
+            rvArticles.setHasFixedSize(true)
+            rvArticles.layoutManager = LinearLayoutManager(requireContext())
+            rvArticles.adapter = articleAdapter
+        }
     }
 
     override fun onDestroy() {
--- favorite/src/main/java/com/app/favorite/ListFavoriteViewModel.kt
+++ favorite/src/main/java/com/app/favorite/ListFavoriteViewModel.kt
@@ -1,8 +1,9 @@
 package com.app.favorite
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.app.core.domain.usecase.ArticleUseCase
 
 class ListFavoriteViewModel(private val articleUseCase: ArticleUseCase) : ViewModel() {
-    // TODO: Implement the ViewModel
+    fun getFavoriteArticles() = articleUseCase.getFavoriteArticles().asLiveData()
 }
