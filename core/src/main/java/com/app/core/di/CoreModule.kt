e18a297f6099cc140d2e26cd06ff8237f570db65
ahmadfma <56568775+ahmadfma@users.noreply.github.com>
2022-09-05 13:53:21 +0800

    set security

--- core/src/main/java/com/app/core/di/CoreModule.kt
+++ core/src/main/java/com/app/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.app.core.data.source.remote.RemoteDataSource
 import com.app.core.data.source.remote.network.ApiService
 import com.app.core.domain.repository.IArticleRepository
 import com.app.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,19 +22,30 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<ArticleDatabase>().articleDao() }
     single {
+        val passPhrase = SQLiteDatabase.getBytes("ahmadfma".toCharArray())
+        val factory = SupportFactory(passPhrase)
         Room.databaseBuilder(
             androidContext(),
             ArticleDatabase::class.java, "Article.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "newsapi.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/7xmA6N1F1gp6ikj57Bg4DMG0jfUB+mZsEL4mZO0qbfU=")
+            .add(hostname, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .add(hostname, "sha256/Y9mvm0exBk1JoQ57f9Vm28jKo5lFm/woKcVxrYxu80o=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
